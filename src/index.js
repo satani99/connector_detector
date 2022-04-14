7ff8b54e8691cc51ee98516f04d061357407de15
satani99 <42287151+satani99@users.noreply.github.com>
2022-04-14 19:25:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ const threshold = 0.75;
 async function load_model() {
     // It's possible to load the model locally or from a repo
     // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
-    const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
-    // const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
+//      const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
+    const model = await loadGraphModel("https://github.com/satani99/connector_detector/blob/main/models/connector-detector/model.json");
     return model;
   }
 
 let classesDir = {
     1: {
-        name: 'Kangaroo',
+        name: 'Connector',
         id: 1,
     },
     2: {
